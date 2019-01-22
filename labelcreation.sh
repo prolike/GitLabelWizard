4d852c5a13463eb59232e82b9d5c8ce6687223c5
Andreas Styltsvig <ansty93@gmail.com>
2019-01-22 10:39:30 +0100

    close #7 read from yml and create label

--- labelcreation.sh
+++ labelcreation.sh
@@ -4,19 +4,18 @@
 . parse_yaml.sh
 
 # read yaml file
-eval $(parse_yaml zconfig.yml "config_")
+eval $(parse_yaml .label.yml "config_")
 
 # access yaml content
 name=$config_label1_name
 color=$config_label1_color
 description=$config_label1_description
 
-curl --request POST \
-  --url https://api.github.com/repos/prolike/dilly-dally/labels \
-   -u Ansty93
-  --header 'cache-control: no-cache' \
-  --header 'content-type: application/json' \
-  --header 'postman-token: 78c3500f-1286-bdbd-ab8f-0ba65bd5378f' \
-  --data '{"name":'$name',"color":'$color',"description":'$description'}'
+#Doing curl POST with json data
+curl -X POST \
+  -H 'authorization: Basic YW5zdHk5MzpTb2x2ZTk5R0g=' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -d '{"name":"'"$name"'","color":"'"$color"'","description":"'"$description"'"}' https://api.github.com/repos/prolike/gitlabelwizard/labels
 
 read -rn1
