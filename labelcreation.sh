791384d5b534341c34a02d7a3d435030442e0fb8
Andreas Styltsvig <ansty93@gmail.com>
2019-01-22 10:52:37 +0100

    close #6 user autherication

--- labelcreation.sh
+++ labelcreation.sh
@@ -10,12 +10,12 @@ eval $(parse_yaml .label.yml "config_")
 name=$config_label1_name
 color=$config_label1_color
 description=$config_label1_description
+read -p 'BasicAuthToken: ' token
 
 #Doing curl POST with json data
 curl -X POST \
-  -H 'authorization: Basic YW5zdHk5MzpTb2x2ZTk5R0g=' \
+  -H 'authorization: Basic "'"$token"'"' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -d '{"name":"'"$name"'","color":"'"$color"'","description":"'"$description"'"}' https://api.github.com/repos/prolike/gitlabelwizard/labels
 
-read -rn1
