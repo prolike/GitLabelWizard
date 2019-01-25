c228c5fe9640c14d23da0180f3dca693c71b3bdc
Laura <tass2012@gmail.com>
2019-01-25 14:24:21 +0100

    Added methods to save text as YML and parse YML to JSON string

--- file_handler.py
+++ file_handler.py
@@ -1,7 +1,12 @@
 # file_handler.py
 # Author: Ansty93 - ansty93@gmail.com
+# Author: cph-lh
 import json
+from ruamel.yaml import YAML
 # Handling the files, saving and loading
+
+#Prevents execution of arbitary code from YAML file - might not be needed though
+yaml = YAML(typ="safe")
 
 def load_sha():
     try:
@@ -29,5 +34,16 @@ def load_labels_from_json_file(filename):
 def format_deletion_labels(arr_json):
      label["name"] = label["name"].replace(" ","%20")
 
+#Reads a YAML file and returns a JSON string
+def yml_to_json(yml):
+    with open(yml) as f:
+        json_string = json.dumps(yaml.load(f))
+#        print("SUCCESS")
+    return json_string
+
+def save_yml(yml):
+    f = open(".labels.yml","w+")
+    f.write(yml)
+    f.close()
 
         
