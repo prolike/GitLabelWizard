6babe501ac26f18e422de3c50820db26cc597cd9
Andreas Styltsvig <ansty93@gmail.com>
2019-01-25 13:40:55 +0100

    refactoring

--- gitlabelwizard.py
+++ gitlabelwizard.py
@@ -1,58 +1,30 @@
 import json
 import os
-from botocore.vendored import requests
 
-## TODO
-## 1. Replace repoowner with "prolike"
+# Local files import
+import file_handler
+import github_handler
+
 repo_owner = "internshipprolike"
+repo_name = "test"
 
-# Authentication token in environment variables (BASIC)
-token = os.environ['basic_token']
+def is_modified(sha):
+    local_sha = file_handler.load_sha()
+    return sha != local_sha
 
-filename_label_creation = "labels_creation.json"
-filename_label_deletion = "labels_deletion.json"
-
-def load_labels_from_json(filename):
-    with open(filename) as f:
-        arr_json = json.load(f)
-    return arr_json
-    
-def label_deletion(repo_name,label,token):
-    url = "https://api.github.com/repos/"+repo_owner+"/"+repo_name+"/labels/"+label
-    headers = {'Authorization':'Basic '+token}
-    r = requests.delete(url,headers=headers)
-    return r
-
-def label_creation(repo_name,label,token):
-    url = "https://api.github.com/repos/"+repo_owner+"/"+repo_name+"/labels"
-    #label = {"name":"pythonboy3222asd222222","color":"ffffff","description":"Its a test"}
-    headers = {'Content-type': 'application/vnd.github.symmetra-preview+json',
-                'Authorization':'Basic '+token}
-    r = requests.post(url, json=label,headers=headers)
-    return r
-    
 # The main func
 def lambda_handler(event, context):
     repo_name = ""
     try:
         body = json.loads(event['body'])
         repo_name = body["repository"]["name"]
-        action = str(body["action"])
+        repo_owner = body["repository"]["owner"]["login"]
+        action = body["action"]
         if action != "created":
-            raise Exception("ACTION IS NOT CREATED")
+            raise Exception("ACTION IS NOT *CREATED* - ABORTING MISSION")
         else:
-            
-            arr_labels_creation = load_labels_from_json(filename_label_creation)
-            arr_labels_deletion = load_labels_from_json(filename_label_deletion)
-            
-            for label in arr_labels_deletion:
-                label["name"] = label["name"].replace(" ","%20")
-                
-            for label in arr_labels_deletion:
-                r = label_deletion(repo_name,label["name"],token)
-                
-            for label in arr_labels_creation:
-                r = label_creation(repo_name,label,token)
+        	#arr_labels = file_handler.
+            github_handler.update_single_repo(repo_owner,repo_name)
     except Exception as e:
         return {
             'statusCode': 400,
@@ -63,3 +35,10 @@ def lambda_handler(event, context):
             'statusCode': 200,
             'body': json.dumps("OK")
         }
+
+def test():
+    arr_labels = file_handler.load_labels_from_json_file("labels_creation.json")
+    arr_labels_delete = file_handler.load_labels_from_json_file("labels_deletion.json")
+    github_handler.insert_labels_repo(repo_owner,repo_name,arr_labels)
+    github_handler.delete_labels_repo(repo_owner,repo_name,arr_labels_delete)
+test()
