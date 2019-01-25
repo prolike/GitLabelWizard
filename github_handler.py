717122fdc75bb863eceba06e9abf43982e045451
Laura <tass2012@gmail.com>
2019-01-25 14:22:28 +0100

    Added method to read from GitHub repo via GitHub API

--- github_handler.py
+++ github_handler.py
@@ -7,11 +7,11 @@ import os
 import json
 
 api_url = "https://api.github.com"
+file_url = "https://raw.githubusercontent.com"
 os.environ["BASIC_TOKEN"] = "YW5zdHk5MzpTb2x2ZTk5R0g"
 # Authentication token in environment variables (BASIC)
 token = os.environ['BASIC_TOKEN']
 label_config_filename = ".labels.yml"
-
 
 
 # Load the sha hash from the latest commit for a given file
@@ -26,7 +26,6 @@ def get_all_repos_in_organization(organization_name):
     return r
 
 
-
 def insert_labels_repo(repo_owner,repo_name,arr_labels):
     for label in arr_labels:
         label_handler.label_insert(api_url,repo_owner,repo_name,label,token)
@@ -34,6 +33,10 @@ def insert_labels_repo(repo_owner,repo_name,arr_labels):
 def delete_labels_repo(repo_owner,repo_name,arr_labels):
     for label in arr_labels:
         label_handler.label_remove(api_url,repo_owner,repo_name,label,token)
+
+def read_yml_from_repo(repo_owner,repo_name):
+    r = requests.get(file_url+"/"+repo_owner+"/"+repo_name+"/master/"+label_config_filename)
+    return r
 
 # def update_single_repo(repo_owner,repo_name,arr_labels):
 #     for label in arr_labels["label_creation"]:
