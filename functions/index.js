f04fde2e216fd4a9c00226bea42cf24b7b5e4eaf
Andreas Styltsvig <ansty93@gmail.com>
2019-02-06 13:39:31 +0100

    Adding new files

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,6 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.helloWorld2 = functions.https.onRequest((request, response) => {
- response.send("TESADAD from Firebase!#2");
-});
-
-exports.helloWorld3 = functions.https.onRequest((request, response) => {
- response.send("TESADAD from Firebase!#2");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
