498c1fbe42023de1cf514110dba09cbacf309f5a
Andreas Styltsvig <ansty93@gmail.com>
2019-02-07 16:20:08 +0100

    Adding sinon framework for testing

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,4 +1,3 @@
-const test = require('firebase-functions-test')();
 const myFunctions = require('../index.js');
 var expect = require('chai').expect
 var request = require('request')
@@ -10,7 +9,6 @@ describe('Test', function(done) {
       // Invoke addMessage with our fake request and response objects. This will cause the
       // assertions in the response object to be evaluated.
       var object = { status: function(){}};
-
       const resMock = {"send":sinon.spy(),"status":sinon.spy(object,"status")} //Format af response ja, vi skal enten mocke eller stubb response det finder vi nok ud af snart, 
       myFunctions.callMe(req,resMock)
       sinon.assert.calledOnce(resMock["send"])
