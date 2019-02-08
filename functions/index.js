3e614385f0262461a5c5470ca71d7bf416f9fe5a
Andreas Styltsvig <ansty93@gmail.com>
2019-02-08 09:38:28 +0100

    close #95 testsetup a basic working test example with http

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,15 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.callMe = functions.https.onRequest((request, response) => {
-	console.log(request)
-	console.log(response)
-		
-		var body_json = getInfo(request.body)
+		//console.log(request)
+		//console.log(response)
+
+		if (request.method != 'POST'){
+			 response.status(403);
+			 return response.send('Forbidden!');
+		}
 		response.status(200)
-		return response.send('test'); //det vi skal ha løst er at finde ud af hvordan vi kan få de ttil at fungere
+		return response.send('test'); 
 	});
 
 
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,25 +1,37 @@
 const myFunctions = require('../index.js');
 var expect = require('chai').expect
-var request = require('request')
 var sinon = require('sinon');
-const req = {"text": 'input'};
+const httpMocks       = require('node-mocks-http');
+const eventEmitter    = require('events').EventEmitter;
 
 describe('Test', function(done) {
- it('should return 200 and test', function (done) {
-      // Invoke addMessage with our fake request and response objects. This will cause the
-      // assertions in the response object to be evaluated.
-      var object = { status: function(){}};
-      const resMock = {"send":sinon.spy(),"status":sinon.spy(object,"status")} //Format af response ja, vi skal enten mocke eller stubb response det finder vi nok ud af snart, 
-      myFunctions.callMe(req,resMock)
-      sinon.assert.calledOnce(resMock["send"])
-      expect
-      console.log(resMock["send"])
-      console.log(resMock["send"]["args"])
-      console.log(resMock["status"]["args"])
+ 
+// https://stackoverflow.com/questions/49352060/how-do-you-unit-test-a-firebase-function-wrapped-with-express
+  it('Method = GET, Should return 403', function (done) {
+      let options = {
+      method: 'GET'
+      };
+      var req = httpMocks.createRequest(options);
+      var res = httpMocks.createResponse({eventEmitter:eventEmitter});
+      myFunctions.callMe(req,res);
+      expect(res.statusCode).to.equal(403)
+      console.log(res)
       done();
     });
 
- 
+   it('Method = POST, Should return 200', function (done) {
+      let options = {
+      method: 'POST',
+      body: '{"test":"test"}'
+      };
+      var req = httpMocks.createRequest(options);
+      var res = httpMocks.createResponse({eventEmitter:eventEmitter});
+      myFunctions.callMe(req,res);
+      expect(res.statusCode).to.equal(200)
+      console.log(res)
+      done();
+    });
+
   });
 
 
