5fe7212fd1142011f12f156101800402e170dbce
Andreas Styltsvig <ansty93@gmail.com>
2019-02-08 15:58:06 +0100

    Adding nock as framework

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,33 @@
 const functions = require('firebase-functions');
+const request = require('request');
 
+api = "https://api.github.com/repos";
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.callMe = functions.https.onRequest((request, response) => {
 		//console.log(request)
 		//console.log(response)
-
 		if (request.method !== 'POST'){
 			 response.status(403);
 			 return response.send('Forbidden!');
 		}
 		response.status(200)
-		return response.send('test'); 
+		return response.send('OK'); 
 	});
 
+exports.addLabel = function(apiUrl,repoOwner,repoName, labelObject, token) {
+	var urlLabel = apiUrl+'/rep os/'+repoOwner+"/"+repoName+"/labels";
 
+	const options = {
+	  method: 'POST',
+	  url: apiUrl,
+	  body: "{'Test':'asd'}"
+	};
 
-function getInfo(body){
-	return body
-}
-
-
-function makeUppercase(data){
-	return data
-}
+	request(options, function (error, response, body) {
+	  console.log('error:', error); // Print the error if one occurred
+	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  console.log('body:', body); // Print the HTML for the Google homepage.
+	});
+} 
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,40 +1,66 @@
 const myFunctions = require('../index.js');
+const test = require('firebase-functions-test')();
 var expect = require('chai').expect
 var sinon = require('sinon');
 const httpMocks       = require('node-mocks-http');
 const eventEmitter    = require('events').EventEmitter;
-
+const nock = require('nock')
+var request = require('request');
 describe('Test', function(done) {
  
 // https://stackoverflow.com/questions/49352060/how-do-you-unit-test-a-firebase-function-wrapped-with-express
   it('Method = GET, Should return 403', function (done) {
-      let options = {
-      method: 'GET'
-      };
-      var req = httpMocks.createRequest(options);
-      var res = httpMocks.createResponse({eventEmitter:eventEmitter});
-      myFunctions.callMe(req,res);
-      expect(res.statusCode).to.equal(403)
-      console.log(res)
-      done();
-    });
-
-   it('Method = POST, Should return 200', function (done) {
-      let options = {
-      method: 'POST',
-      body: '{"test":"test"}'
-      };
-      var req = httpMocks.createRequest(options);
-      var res = httpMocks.createResponse({eventEmitter:eventEmitter});
-      myFunctions.callMe(req,res);
-      expect(res.statusCode).to.equal(200)
-      console.log(res)
-      done();
-    });
-
+    let options = {
+      method: 'GET'};
+    var req = httpMocks.createRequest(options);
+    var res = httpMocks.createResponse({eventEmitter:eventEmitter});
+    myFunctions.callMe(req,res);
+    expect(res.statusCode).to.equal(403)
+    //console.log(res)
+    done();
   });
 
+  it('Method = POST, Should return 200', function (done) {
+    let options = {
+      method: 'POST',
+      body: '{"test":"test"}'};
+    var req = httpMocks.createRequest(options);
+    var res = httpMocks.createResponse({eventEmitter:eventEmitter});
+    myFunctions.callMe(req,res);
+    expect(res.statusCode).to.equal(200)
+    //console.log(res)
+    done();
+  });
+
+ 
+});
+
+describe('labels test', function(done) {
+  
 
 
+it('Method = POST, Mock server', function (done) {
+     const scope = nock('https://api.github.com')
+    .post('/repos/prolike/gitlabelwizard/labels')
+    .reply(function(uri, requestBody) {
+      console.log('path:', this.req.path)
+      console.log('headers:', this.req.headers)
+      console.log('headers:', requestBody)
+      expect(requestBody).to.equal("{'Test':'asd'}")
+
+     // ...
+    })
+    .log(console.log)
+
+    var repoOwner = "prolike"
+    var repoName = "gitlabelwizard"
+    var apiUrl = 'https://api.github.com/repos/prolike/gitlabelwizard/labels'
+    var token = "tokenasdasdasd"
+    var labelObject = "{'name': 'test label', 'color': 'ffffff'}"
+    myFunctions.addLabel(apiUrl,repoOwner,repoName,labelObject,token);
+   
+    done();
+  });
+});
 
 
